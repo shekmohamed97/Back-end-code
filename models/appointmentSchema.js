6c85ec24a2c51bb2b8204f8d5849a9504e7bb874
shekmohamed97 <shek37576@gamail.com>
2024-07-15 02:10:23 +0530

    update success

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -43,42 +43,12 @@ const appointmentSchema = new mongoose.Schema({
         type: String,
         required: [true, "Appointment Date Is Required!"],
       },
-      department: {
-        type: String,
-        required: [true, "Department Name Is Required!"],
-      },
-      doctor: {
-        firstName: {
-          type: String,
-          required: [true, "Doctor Name Is Required!"],
-        },
-        lastName: {
-          type: String,
-          required: [true, "Doctor Name Is Required!"],
-        },
-      },
-      hasVisited: {
-        type: Boolean,
-        default: false,
-      },
+      
       address: {
         type: String,
         required: [true, "Address Is Required!"],
       },
-      doctorId: {
-        type: mongoose.Schema.ObjectId,
-        required: [true, "Doctor Id Is Invalid!"],
-      },
-      patientId: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: [true, "Patient Id Is Required!"],
-      },
-      status: {
-        type: String,
-        enum: ["Pending", "Accepted", "Rejected"],
-        default: "Pending",
-      },
+      
 });
 
 export const Appointment= mongoose.model("Appointment",appointmentSchema)
