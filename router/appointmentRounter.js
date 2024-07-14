32ebd9fc209eec6435a25faf4e73bc865622461f
shekmohamed97 <shek37576@gamail.com>
2024-07-15 01:03:54 +0530

    small update

--- router/appointmentRounter.js
+++ router/appointmentRounter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controllers/appointmentController.js";
-import {isAdminAuthendicated,isPatientAuthendicated} from "../middlewear/authentication.js"
+import {isAdminAuthendicated} from "../middlewear/authentication.js"
 
 const router = express.Router();
 
-router.post("/post",isPatientAuthendicated,postAppointment);
+router.post("/post",postAppointment);
 router.get("/getall",isAdminAuthendicated,getAllAppointments)
 router.put("/update/:id",isAdminAuthendicated,updateAppointmentStatus);
 router.delete("/delete/:id",isAdminAuthendicated,deleteAppointment);
